e2fae696950f6c7083c35cd365b62ce5ac628330
Naveen Raji Eapen <raji.nav1999@gmail.com>
2026-02-21 12:46:40 -0700

    restrict CORS to vercal + localhost

--- backend/main.py
+++ backend/main.py
@@ -51,7 +51,10 @@ app = FastAPI(title="CRM Deals Consolidation", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://crm-deals-consolidator.vercel.app",],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
